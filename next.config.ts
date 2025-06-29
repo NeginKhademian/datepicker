a09ecc7202f838dd75e78b6d026ddf1c4df59af7
negin.khademian <neginkhademian@gmail.com>
2025-06-29 14:45:18 +0330

    fix: update gitignore and nextconfig

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,8 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/" : undefined,
-  assetPrefix: isProd ? "/" : undefined,
-  // …other settings
+
+  // …
 };
 
 
