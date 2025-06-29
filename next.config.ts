11d734ac9bc10818de5cb067755b516c11a55ada
negin.khademian <neginkhademian@gmail.com>
2025-06-29 14:15:32 +0330

    fix: update base and assest path

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,8 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isProd ? "/datepicker" : undefined,
-  assetPrefix: isProd ? "/datepicker/" : undefined,
+  basePath: isProd ? "/" : undefined,
+  assetPrefix: isProd ? "//" : undefined,
   // …other settings
 };
 
